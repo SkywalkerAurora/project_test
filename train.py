18cb2de9d6466abc4a4bfc4dafd63c33068a8a9d
Skywalker <347468420@qq.com>
2024-10-09 22:16:53 +0800

    新建了train.py文件

---
+++ train.py
@@ -0,0 +1,23 @@
+import warnings
+warnings.filterwarnings('ignore')
+import torch
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/v8/yolov8-bifpn.yaml')
+    model.load('yolov8n.pt') # loading pretrain weights
+    model.train(data='dataset/data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=100,
+                batch=16,
+                close_mosaic=10,
+                workers=4,
+                device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu"),
+                optimizer='SGD', # using SGD
+                # resume='', # last.pt path
+                # amp=False, # close amp
+                # fraction=0.2,
+                project='runs/train',
+                name='exp',
+                )
